aa9b78f139ca91d4ea4f75cf7ff27fba2aec7cb9
Nikita Striuk <nikita.striuk@datarobot.com>
2021-03-31 21:48:31 +0300

    spinner used

--- src/app/AppProviders/AuthProvider/AuthProviderLoading.js
+++ src/app/AppProviders/AuthProvider/AuthProviderLoading.js
@@ -1,17 +1,13 @@
-import { useTranslation } from 'react-i18next';
-
-import { DisplayLarge } from 'baseui/typography';
+import { Spinner } from 'baseui/spinner';
 
 import { StyledApp } from 'app/StyledApp';
 import { StyledPage } from 'styles/StyledPage';
 
 export function AuthProviderLoading() {
-  const { t } = useTranslation();
-
   return (
     <StyledApp>
       <StyledPage style={{ height: '100vh' }}>
-        <DisplayLarge>{t('global.loading')}</DisplayLarge>
+        <Spinner />
       </StyledPage>
       ;
     </StyledApp>
