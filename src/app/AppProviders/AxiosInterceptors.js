4891139644ee57564fd7f8e6597eef854018157e
Nikita Striuk <nikita.striuk@datarobot.com>
2021-04-05 23:57:43 +0300

    AxiosInterceptors tests

--- src/app/AppProviders/AxiosInterceptors.js
+++ src/app/AppProviders/AxiosInterceptors.js
@@ -6,9 +6,9 @@ import { useAuthActions } from 'hooks/useAuthActions';
 import axios from 'utils/axios';
 import ls from 'utils/localStorage';
 
-const TOKEN_EXPIRED_STATUS_CODE = 403;
-const UNAUTHORIZED_STATUS_CODE = 401;
-const AUTHORIZATION_TOKEN_PREFIX = 'Bearer';
+export const TOKEN_EXPIRED_STATUS_CODE = 403;
+export const UNAUTHORIZED_STATUS_CODE = 401;
+export const AUTHORIZATION_TOKEN_PREFIX = 'Bearer';
 
 // Accessing private resource:
 // 1. Valid access token - access granted
@@ -22,50 +22,66 @@ export function AxiosInterceptors() {
   useEffect(() => {
     // Set request interceptors for
     const setAuthorizationHeaderInterceptor = axios.interceptors.request.use(
-      async (config) => {
-        const accessToken = ls.getItem(ACCESS_TOKEN_LS_KEY);
-        if (accessToken) {
-          // eslint-disable-next-line no-param-reassign
-          config.headers = {
-            Authorization: `${AUTHORIZATION_TOKEN_PREFIX} ${accessToken}`,
-          };
-        }
-        return config;
-      },
-      (error) => {
-        Promise.reject(error);
-      },
+      makeSetAuthorizationHeaderInterceptor(),
+      (error) => Promise.reject(error),
     );
 
     // Set response interceptors
+    const unauthorizedResponseInterceptor = axios.interceptors.response.use(
+      (response) => response,
+      makeUnauthorizedResponseInterceptorErrorHandler(logoutUser),
+    );
     const refreshUserTokenInterceptor = axios.interceptors.response.use(
-      (response) => {
-        return response;
-      },
-      async (error) => {
-        const originalRequest = error.config;
-        if (error?.response?.status === UNAUTHORIZED_STATUS_CODE) {
-          return logoutUser();
-        }
-        if (error?.response?.status === TOKEN_EXPIRED_STATUS_CODE && !originalRequest._retry) {
-          originalRequest._retry = true;
-          const refreshToken = ls.getItem(REFRESH_TOKEN_LS_KEY);
-          if (refreshToken) {
-            axios.defaults.headers.common.Authorization = `${AUTHORIZATION_TOKEN_PREFIX} ${await refreshUserToken()}`;
-            return axios(originalRequest);
-          }
-        }
-        return Promise.reject(error);
-      },
+      (response) => response,
+      makeRefreshUserTokenInterceptorErrorHandler(refreshUserToken),
     );
 
     return () => {
       // Eject request interceptors
       axios.interceptors.request.eject(setAuthorizationHeaderInterceptor);
+
       // Eject response interceptors
+      axios.interceptors.response.eject(unauthorizedResponseInterceptor);
       axios.interceptors.response.eject(refreshUserTokenInterceptor);
     };
   }, [logoutUser, refreshUserToken]);
 
   return null;
+}
+
+export function makeSetAuthorizationHeaderInterceptor() {
+  return (config) => {
+    const accessToken = ls.getItem(ACCESS_TOKEN_LS_KEY);
+    if (accessToken) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers = {
+        Authorization: `${AUTHORIZATION_TOKEN_PREFIX} ${accessToken}`,
+      };
+    }
+    return config;
+  };
+}
+
+export function makeUnauthorizedResponseInterceptorErrorHandler(logoutUser) {
+  return (error) => {
+    if (error?.response?.status === UNAUTHORIZED_STATUS_CODE) {
+      return logoutUser();
+    }
+    return Promise.reject(error);
+  };
+}
+
+export function makeRefreshUserTokenInterceptorErrorHandler(refreshUserToken) {
+  return async (error) => {
+    const originalRequest = error.config;
+    if (error?.response?.status === TOKEN_EXPIRED_STATUS_CODE && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = ls.getItem(REFRESH_TOKEN_LS_KEY);
+      if (refreshToken) {
+        axios.defaults.headers.common.Authorization = `${AUTHORIZATION_TOKEN_PREFIX} ${await refreshUserToken()}`;
+        return axios(originalRequest);
+      }
+    }
+    return Promise.reject(error);
+  };
 }
