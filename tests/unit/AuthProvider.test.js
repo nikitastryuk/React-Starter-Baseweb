abfb14d8eb4f3d5b4778d01c21f2efe984216cb0
Nikita Striuk <nikita.striuk@datarobot.com>
2021-04-07 21:25:02 +0300

    AuthActionsContext test

--- tests/unit/AuthProvider.test.js
+++ tests/unit/AuthProvider.test.js
@@ -1,29 +1,45 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
+import { AuthActionsContext, AuthProvider, AuthStateContext } from 'app/AppProviders/AuthProvider/AuthProvider';
 import { AuthApi } from 'app/AppProviders/AuthProvider/authApi';
-import { AuthProvider, AuthStateContext } from 'app/AppProviders/AuthProvider/AuthProvider';
 import ls from 'utils/localStorage';
 
 jest.mock('react-i18next');
 
-function renderWithProviders() {
-  return render(
-    <AuthProvider>
-      <AuthStateContext.Consumer>
-        {(value) => <p data-testid="auth-provider-state-value">{value}</p>}
-      </AuthStateContext.Consumer>
-    </AuthProvider>,
+function TestComponent() {
+  return (
+    <AuthStateContext.Consumer>
+      {(user) => (
+        <AuthActionsContext.Consumer>
+          {({ loginUser, refreshUserToken, logoutUser }) => (
+            <>
+              <p data-testid="auth-provider-state-value">{user}</p>
+              <button onClick={loginUser}>Login</button>
+              <button onClick={refreshUserToken}>Refresh token</button>
+              <button onClick={logoutUser}>Logout</button>
+            </>
+          )}
+        </AuthActionsContext.Consumer>
+      )}
+    </AuthStateContext.Consumer>
   );
 }
 
+function renderWithProviders() {
+  return render(<TestComponent />, { wrapper: AuthProvider });
+}
+
 describe('<AuthProvider />', () => {
+  beforeEach(() => {
+    jest.spyOn(ls, 'getItem').mockReturnValue(null);
+  });
   it('should have empty initial state', async () => {
     const { getByTestId } = renderWithProviders();
     expect(getByTestId('auth-provider-state-value').textContent).toBe('');
   });
 
   it('should not load user if auth tokens are not stored', async () => {
-    jest.spyOn(ls, 'getItem').mockReturnValue(null);
     const { queryByTestId } = renderWithProviders();
     expect(queryByTestId('auth-provider-loading-spinner')).toBeNull();
   });
@@ -36,5 +52,19 @@ describe('<AuthProvider />', () => {
     expect(AuthApi.getUser).toHaveBeenCalledTimes(1);
     expect(getByTestId('auth-provider-loading-spinner')).toBeTruthy();
     expect((await findByTestId('auth-provider-state-value')).textContent).toBe(getUserResponse.data.user);
+  });
+
+  it('should login and logout user', async () => {
+    const loginUserResponse = { data: { user: 'user' } };
+    jest.spyOn(AuthApi, 'loginUser').mockReturnValue(Promise.resolve(loginUserResponse));
+    const { getByRole, findByTestId } = renderWithProviders();
+    userEvent.click(getByRole('button', { name: 'Login' }));
+    expect(AuthApi.loginUser).toHaveBeenCalledTimes(1);
+    expect((await findByTestId('auth-provider-state-value')).textContent).toBe(loginUserResponse.data.user);
+
+    jest.spyOn(AuthApi, 'logoutUser').mockReturnValue(Promise.resolve());
+    userEvent.click(getByRole('button', { name: 'Logout' }));
+    expect(AuthApi.logoutUser).toHaveBeenCalledTimes(1);
+    expect((await findByTestId('auth-provider-state-value')).textContent).toBe('');
   });
 });
